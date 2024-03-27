2ae3e11c465420f4f12d7fbd18f888efaad2fafa
Thibaut Cantet <thibaut.cantet-extern@renault.fr>
2024-03-27 14:56:58 +0100

    add test for coverage

--- src/test/java/org/kata/panier/SubjectUnderTestTest.java
+++ src/test/java/org/kata/panier/SubjectUnderTestTest.java
@@ -25,12 +25,30 @@ class SubjectUnderTestTest {
     }
 
     @Test
+    void should_add_vat_and_taxe_for_expensive_good() {
+        var subjectUnderTest = new SubjectUnderTest();
+
+        var result = subjectUnderTest.execute(1001, 15.0f, false);
+
+        assertThat(result).isEqualTo(1131.13f);
+    }
+
+    @Test
     void should_add_vat_and_imported_taxe_for_cheap_good() {
         var subjectUnderTest = new SubjectUnderTest();
 
         var result = subjectUnderTest.execute(10, 15.0f, true);
 
         assertThat(result).isEqualTo(11.73f);
+    }
+
+    @Test
+    void should_add_vat_and_imported_low_taxe_for_cheap_good() {
+        var subjectUnderTest = new SubjectUnderTest();
+
+        var result = subjectUnderTest.execute(10, 1.0f, true);
+
+        assertThat(result).isEqualTo(10.605f);
     }
 
     @Test
